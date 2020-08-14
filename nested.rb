69770d8db34116e07f484c7e70cd9faa529748b3
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-14 03:33:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,3 +128,25 @@ def adding_to_dennis
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   programmer_hash
 end
+
+def adding_matz
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash
+end
