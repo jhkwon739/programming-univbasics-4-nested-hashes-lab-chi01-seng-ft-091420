ef368ff4e6fa96300c3f1be9429147d97cb31514
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-14 03:34:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -150,3 +150,23 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
 end
+
+def changing_alan
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
+end
